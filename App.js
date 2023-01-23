40e6401f61ef236ee35e7f7b3a8938e81a2f3fc2
Shivendraprajapati <111180105+Shivendraprajapati@users.noreply.github.com>
2023-01-23 13:34:33 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,1 +1,35 @@
 
+
+
+
+
+    let email = document.getElementById('Email');
+    let password = document.getElementById('Password');
+
+function validated() {
+    // e.preventDefault();
+
+    if (email.value.length < 8) {
+		email.style.border = "1px solid red";
+		email_error.style.display = "block";
+		email.focus();
+		return false;
+	}
+    else if (email.value.length >= 9) {
+		email.style.border = "1px solid silver";
+		email_error.style.display = "none";
+		
+	}
+	if (password.value.length < 5) {
+		password.style.border = "1px solid red";
+		pass_error.style.display = "block";
+		password.focus();
+		return false;
+	}
+    else if (password.value.length >= 6) {
+		password.style.border = "1px solid silver";
+		pass_error.style.display = "none";
+		return true;
+	}
+    
+}
